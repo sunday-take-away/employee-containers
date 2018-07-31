028cdb25735de1106f18b108a4c5487e9169f837
Sunday Take Away <eugene.le.roux@icloud.com>
2018-07-31 18:08:42 +0200

    changed employee database + container name + added default credentials

---
+++ database/employee/data/credential.js
@@ -0,0 +1,7 @@
+db.credential.insert({
+    "_id" : ObjectId("5b606730b150fa57a1bc9a1d"),
+    "username" : "admin",
+    "password" : "HackASnack",
+    "accessGroup" : "administrators",
+    "created" : ISODate("2018-07-31T17:54:12.013Z")
+})
